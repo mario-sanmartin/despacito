232baff9cb3f0746a991c4a46fd4d2f1c2732720
mario-sanmartin <sanmartin.mario@outlook.com>
2020-12-06 23:57:39 -0300

    montando servidor con node

---
+++ main.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=> {
+   
+    res.end('Estoy respondiendo a a solicitud V.2')
+})
+
+const puerto = 3000;
+server.listen(puerto,()=>{
+    console.log('Escuchando cumbia')
+})
