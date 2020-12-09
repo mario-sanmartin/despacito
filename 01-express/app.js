97a39b4517756b694981907a8f11f7c5c299de14
mario-sanmartin <sanmartin.mario@outlook.com>
2020-12-09 00:02:48 -0300

    Estructura pagina estatica con Express

--- 01-express/app.js
+++ 01-express/app.js
@@ -4,6 +4,9 @@ const app = express();
 
 const puerto = 3000;
 
+
+app.use(express.static(__dirname + '/public'))
+
 app.get('/',(req,res)=>{
     res.send('Mi respuesta desde Express')
 })
@@ -11,6 +14,8 @@ app.get('/',(req,res)=>{
 app.get('/servicios',(req,res)=>{
     res.send('Estas en la seccion de Servicios')
 })
+
+
 
 app.listen(puerto,()=>{
     console.log('Servidor a su servicio reina',puerto);
