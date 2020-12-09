10983cd9409f1d990b1df55820c32d551061f671
mario-sanmartin <sanmartin.mario@outlook.com>
2020-12-09 00:08:33 -0300

    rutas 404 configurada

--- 01-express/app.js
+++ 01-express/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const puerto = 3000;
 
-
+//Middleware
 app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req,res)=>{
@@ -15,7 +15,10 @@ app.get('/servicios',(req,res)=>{
     res.send('Estas en la seccion de Servicios')
 })
 
-
+//Para que se ejecuten todas las rutas configuradas
+app.use((req,res,next) => {
+    res.status(404).sendFile(__dirname+'/public/404.html')
+})
 
 app.listen(puerto,()=>{
     console.log('Servidor a su servicio reina',puerto);
